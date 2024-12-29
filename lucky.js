cc2984f45d780de283ac73e962ab6152486522b4
xdevolt <danilvkosmose@gmail.com>
2024-12-29 14:40:03 +0200

    g

---
+++ lucky.js
@@ -0,0 +1,23 @@
+const tyalet = document.getElementById("tyalet");
+
+
+tyalet.addEventListener("click", ()=>{
+    tyalet.style.width = "200px";
+    tyalet.style.height = "200px";
+
+});
+const did = document.getElementById("did");
+
+
+did.addEventListener("click", ()=>{
+    did.style.width = "200px";
+    did.style.height = "200px";
+
+});
+const ok = document.getElementById("ok");
+
+
+ok.addEventListener("click", ()=>{
+    ok.style.width = "200px";
+    ok.style.height = "150px";
+});
---
+++ mirage.js
@@ -0,0 +1,7 @@
+const reklama = document.getElementById("reklama");
+
+
+reklama.addEventListener("click",  ()=>{
+    reklama.style.width = "300px";
+    reklama.style.height = "400px";
+})
